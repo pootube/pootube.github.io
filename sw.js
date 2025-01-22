9d2833707f4685b5c6445d1a6392b35df4c4cc5c
pootube <yoriolewis@gmail.com>
2025-01-21 22:23:03 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+// sw.js
+const CACHE_NAME = 'youtube-pwa-v1';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/icon.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
